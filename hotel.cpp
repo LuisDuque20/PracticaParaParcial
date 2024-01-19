277c5d59a6aaf6d2d194e220f07444f90c9d2f3a
LuisDuque20 <00013423@uca.edu.sv>
2024-01-18 20:46:37 -0600

    style: :art: Creando estructura del codigo base

---
+++ hotel.cpp
@@ -0,0 +1,21 @@
+/* Escribir una funcion que devuelva multiples valores a traves del uso de
+referencias, que solicite al usuario el numero de dias
+y el tipo de habitacion en la que se hospedara en el hotel.
+Calcular (valores a devolver) el precio por noche, el impuesto a pagar
+y el precio total que el usuario debera cancelar
+Toma en cuenta los precios por el tipo de habitacion.
+-Sencilla 45.00
+-Doble ejecutiva 70.00
+-Suite presidencial 150.00          
+
+Finalmente mostrar el detalle de la cantidad de noches, el precio por noche,
+el detalle del impuesto y el precio total a pagar por habitacion. */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+    return 0;
+}
---
+++ lib.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+double habSencilla = 45.00,
+       habEjecutiva = 75.00,
+       habPresidencial = 150.00,
+       totalCancelar=0;
+
+int numDias;
+
+void pedirDatos();
