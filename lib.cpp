d357b662ef1b8e6675b7bbfe9d6a8643c2b1f576
LuisDuque20 <00013423@uca.edu.sv>
2024-01-18 21:30:22 -0600

    feat: :sparkles: Creando funcion para pedir los datos al usuario.

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include "lib.hpp"
 using namespace std;
 
-void pedirDatos(){
-
-
-
-
-
+void pedirDatos(DatosHuesped &x)
+{
+    cout << "Cuantos dias estara en el hotel? ";
+    cin >> x.dias;
+    cout << "En que tipo de habitacion le gusataria hospedarse? \n"
+         << "1 --> Habitacion sencilla: $45.00 por noche \n"
+         << "2 --> Habitacion ejecutiva: $70.00 por noche \n"
+         << "3 --Suite residencial: $150.00 \n";
+    cin >> x.op;
+    if (x.op == 1)
+    {
+        cout << "Usted ha seleccionado la habitacion sencilla";
+    }
+    else if (x.op == 2)
+    {
+        cout << "Usted ha seleccionado la habitacion ejecutiva";
+    }
+    else if (x.op == 3)
+    {
+        cout << "Usted ha seleccionado la suite presidencial";
+    }
 }
